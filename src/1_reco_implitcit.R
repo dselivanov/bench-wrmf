b1f445e9f435a5217d4c48424ec1a86f4a039d56
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2017-08-01 20:27:38 +0300

    update according to https://github.com/dselivanov/reco/commit/7a8742602c2bfc1d55b7aaf84db1807962f74d69

--- src/1_reco_implitcit.R
+++ src/1_reco_implitcit.R
@@ -8,10 +8,14 @@ m = m$x
 N_ITER = 3
 RANK = 128L
 als = ALS$new(rank = RANK, lambda = 0.0, feedback = "implicit",
-              init_stdv = 0.01, n_threads = 8)
+              init_stdv = 0.01, n_threads = 8,
+              # solver ="conjugate_gradient",
+              solver ="cholesky",
+              cg_steps = 3L)
 RhpcBLASctl::blas_set_num_threads(1)
 
 t1 = Sys.time()
 futile.logger::flog.info("start")
 temp = als$fit_transform(x = m, n_iter = N_ITER, convergence_tol = -1, n_threads = 8)
+
 difftime(Sys.time(), t1, units = "sec")
